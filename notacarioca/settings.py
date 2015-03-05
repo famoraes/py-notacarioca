7ad94d2624ce50ff6c5fb2cd62d33274cddc9d71
Fabiano Moraes <fabiano.moraes@outlook.com>
2015-03-05 09:24:35 -0300

    Adicionado arquivo de configuração e templates iniciais

---
+++ notacarioca/settings.py
@@ -0,0 +1,14 @@
+URL = {
+    3303302: {
+        "sandbox": "https://notacarioca.rio.gov.br/WSNacional/nfse.asmx?wsdl",
+        "production": "https://homologacao.notacarioca.rio.gov.br/WSNacional/nfse.asmx?wsdl"
+    }
+}
+
+TEMPLATES = {
+    'send_rps': "RecepcionarLoteRps.xml",
+}
+
+METHODS = {
+    'send_rps': "RecepcionarLoteRps",
+}
